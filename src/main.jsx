064d426d395e12fd08ef98125f778f2cc63401d1
Fcbaskan <furkancagribaskan32@gmail.com>
2025-10-30 21:43:28 +0300

    Hata Çözüm

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // İçe aktar
+import {HashRouter } from 'react-router-dom' // İçe aktar
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* Hafta-3'teki bu dosya [cite: 758] artık router içeriyor */}
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
